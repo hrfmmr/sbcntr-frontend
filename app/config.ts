301b28da73aea20144632adf7b243cb4eceedacd
hrfmmr <i@hrfmmr.com>
2022-12-18 10:38:05 +0900

    Ensure backend app can connect to db

--- app/config.ts
+++ app/config.ts
@@ -1,9 +1,9 @@
 export const APP_SERVICE_HOST = process.env.APP_SERVICE_HOST
   ? process.env.APP_SERVICE_HOST
-  : "http://localhost";
+  : "http://localhost:3200";
 
 export const NOTIF_SERVICE_HOST = process.env.NOTIF_SERVICE_HOST
   ? process.env.NOTIF_SERVICE_HOST
-  : "http://localhost";
+  : "http://localhost:3200";
 
 export const DRAWER_WIDTH = 240;
